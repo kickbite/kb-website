2c5264da09d3341161519169c43d4eba357003af
Sergiusz Różycki <sergiusz@iMac-Sergiusz.lan>
2023-12-30 14:35:34 +0100

    update

--- coretechnology.js
+++ coretechnology.js
@@ -16,12 +16,11 @@
 
 let headers = document.querySelectorAll("[data-item='header']");
 let blocks = document.querySelectorAll("[data-item='block']");
-
 let lastBlockIndex = blocks.length - 1;
 
-let observer = new IntersectionObserver((entries, observer) => {
+let observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting && entry.intersectionRatio === 1) {
+    if (entry.isIntersecting && (window.innerWidth > 479 || entry.intersectionRatio === 1)) {
       gsap.to(headers[lastBlockIndex], { onComplete: () => headers[lastBlockIndex].classList.add("auto-margin") });
     } else {
       gsap.to(headers[lastBlockIndex], { onComplete: () => headers[lastBlockIndex].classList.remove("auto-margin") });
@@ -29,4 +28,22 @@ let observer = new IntersectionObserver((entries, observer) => {
   });
 });
 
-observer.observe(blocks[lastBlockIndex]);
+// Observe all blocks for large screens, the last one for small screens
+if (window.innerWidth > 479) {
+  blocks.forEach((block) => observer.observe(block));
+} else {
+  observer.observe(blocks[lastBlockIndex]);
+}
+
+// Resize handler to switch mode if required
+window.addEventListener("resize", () => {
+  let largeScreenMode = window.innerWidth > 479;
+
+  if (largeScreenMode) {
+    observer.disconnect();
+    blocks.forEach((block) => observer.observe(block));
+  } else {
+    observer.disconnect();
+    observer.observe(blocks[lastBlockIndex]);
+  }
+});
