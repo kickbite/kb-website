eabd5cc0329f00f49360f5e415639b78a87ed2f7
Sergiusz Różycki <sergiusz@iMac-Sergiusz.lan>
2024-01-03 12:46:05 +0100

    resize

--- coretechnology.js
+++ coretechnology.js
@@ -1,15 +1,43 @@
+// let headers = document.querySelectorAll("[data-item='header']");
+// let blocks = document.querySelectorAll("[data-item='block']");
+
+// blocks.forEach((block, index) => {
+//   let observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         gsap.to(headers[index], { onComplete: () => headers[index].classList.add("auto-margin") });
+//       } else {
+//         gsap.to(headers[index], { onComplete: () => headers[index].classList.remove("auto-margin") });
+//       }
+//     });
+//   });
+//   observer.observe(block);
+// });
+
 let headers = document.querySelectorAll("[data-item='header']");
 let blocks = document.querySelectorAll("[data-item='block']");
 
-blocks.forEach((block, index) => {
-  let observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        gsap.to(headers[index], { onComplete: () => headers[index].classList.add("auto-margin") });
-      } else {
-        gsap.to(headers[index], { onComplete: () => headers[index].classList.remove("auto-margin") });
-      }
+function observeBlocks() {
+  blocks.forEach((block, index) => {
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          gsap.to(headers[index], { onComplete: () => headers[index].classList.add("auto-margin") });
+        } else {
+          gsap.to(headers[index], { onComplete: () => headers[index].classList.remove("auto-margin") });
+        }
+      });
     });
+    observer.observe(block);
   });
-  observer.observe(block);
+}
+
+if (window.innerWidth > 767) {
+  observeBlocks();
+}
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 767) {
+    observeBlocks();
+  }
 });
