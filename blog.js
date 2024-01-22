15ebe3d6045431eaf4ee0213f385ccb92de6af7b
Sergiusz Różycki <sergiusz@iMac-Sergiusz.local>
2024-01-22 08:53:52 +0100

    blog update

--- blog.js
+++ blog.js
@@ -1,129 +1,129 @@
-$(document).ready(function () {
-  var sb = $('[data-item="search-bar"]');
-  var sBox = $('[data-item="search-box"]');
-  var sClose = $('[data-item="search-close"]');
+// $(document).ready(function () {
+//   var sb = $('[data-item="search-bar"]');
+//   var sBox = $('[data-item="search-box"]');
+//   var sClose = $('[data-item="search-close"]');
 
-  if (!sb.is(":focus")) sClose.hide();
+//   if (!sb.is(":focus")) sClose.hide();
 
-  sb.on("focus", function () {
-    if ($(window).width() > 767) {
-      $(this).add(sBox).addClass("is-active");
-      sClose.show();
-    }
-  });
+//   sb.on("focus", function () {
+//     if ($(window).width() > 767) {
+//       $(this).add(sBox).addClass("is-active");
+//       sClose.show();
+//     }
+//   });
 
-  sb.on("blur", function () {
-    if ($(window).width() > 767) {
-      $(this).add(sBox).removeClass("is-active").end().val("");
-      sClose.hide();
-    }
-  });
+//   sb.on("blur", function () {
+//     if ($(window).width() > 767) {
+//       $(this).add(sBox).removeClass("is-active").end().val("");
+//       sClose.hide();
+//     }
+//   });
 
-  sClose.on("click", function () {
-    if ($(window).width() > 767) sb.val("").blur();
-  });
+//   sClose.on("click", function () {
+//     if ($(window).width() > 767) sb.val("").blur();
+//   });
 
-  $(document).on("keypress", function (e) {
-    if (e.which === 47 && $(window).width() > 767) {
-      e.preventDefault();
-      sb.focus();
-    }
-  });
-});
+//   $(document).on("keypress", function (e) {
+//     if (e.which === 47 && $(window).width() > 767) {
+//       e.preventDefault();
+//       sb.focus();
+//     }
+//   });
+// });
 
-$(document).ready(function () {
-  var element = $('[fs-cmsfilter-element="clear"]').remove();
-  element.prependTo('[data-item="fiters"]');
-});
+// $(document).ready(function () {
+//   var element = $('[fs-cmsfilter-element="clear"]').remove();
+//   element.prependTo('[data-item="fiters"]');
+// });
 
-$(document).ready(function () {
-  var clearElement = $('[fs-cmsfilter-element="clear"]');
-  var checkboxes = $('[data-item="fiters"] input[type="checkbox"]').not(clearElement.find("input"));
-  var searchBar = $('[data-item="search-bar"]');
-  var queryElement = $('[data-item="search-query"]');
-  var listElement = $('[fs-cmsfilter-element="list"]');
-  var filtersState = 0;
+// $(document).ready(function () {
+//   var clearElement = $('[fs-cmsfilter-element="clear"]');
+//   var checkboxes = $('[data-item="fiters"] input[type="checkbox"]').not(clearElement.find("input"));
+//   var searchBar = $('[data-item="search-bar"]');
+//   var queryElement = $('[data-item="search-query"]');
+//   var listElement = $('[fs-cmsfilter-element="list"]');
+//   var filtersState = 0;
 
-  var config = {
-    attributes: true,
-    childList: true,
-    subtree: true,
-    attributeFilter: ["class"],
-    attributeOldValue: true,
-  };
+//   var config = {
+//     attributes: true,
+//     childList: true,
+//     subtree: true,
+//     attributeFilter: ["class"],
+//     attributeOldValue: true,
+//   };
 
-  var observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      if (mutation.type === "attributes") {
-        if ($(mutation.target).is("label.is-active")) {
-          filtersState += 1;
-          if ($('[data-item="list-all"]').css("display") !== "grid" && mutation.target !== clearElement.get(0)) {
-            clearElement.removeClass("is-active");
-          }
-        } else if (mutation.oldValue.includes("is-active")) {
-          filtersState = Math.max(0, filtersState - 1);
-        }
-      }
-    });
-  });
+//   var observer = new MutationObserver(function (mutations) {
+//     mutations.forEach(function (mutation) {
+//       if (mutation.type === "attributes") {
+//         if ($(mutation.target).is("label.is-active")) {
+//           filtersState += 1;
+//           if ($('[data-item="list-all"]').css("display") !== "grid" && mutation.target !== clearElement.get(0)) {
+//             clearElement.removeClass("is-active");
+//           }
+//         } else if (mutation.oldValue.includes("is-active")) {
+//           filtersState = Math.max(0, filtersState - 1);
+//         }
+//       }
+//     });
+//   });
 
-  var targetNode = $('[data-item="fiters"]').get(0);
-  observer.observe(targetNode, config);
+//   var targetNode = $('[data-item="fiters"]').get(0);
+//   observer.observe(targetNode, config);
 
-  function updateCountAndQuery() {
-    setTimeout(function () {
-      var resultsCount = listElement.children().length;
-      var searchValue = searchBar.val();
-      queryElement.text(`${searchValue}`);
+function updateCountAndQuery() {
+  setTimeout(function () {
+    // var resultsCount = listElement.children().length;
+    var searchValue = searchBar.val();
+    queryElement.text(`${searchValue}`);
 
-      adjustClearElementClass();
-    }, 200);
-  }
+    // adjustClearElementClass();
+  }, 200);
+}
 
-  function adjustClearElementClass() {
-    if (!Object.values(filtersState).includes(true)) {
-      if ($('[data-item="list-all"]').css("display") !== "grid") {
-        clearElement.addClass("is-active");
-      }
-    } else {
-      clearElement.removeClass("is-active");
-    }
-  }
+// function adjustClearElementClass() {
+//   if (!Object.values(filtersState).includes(true)) {
+//     if ($('[data-item="list-all"]').css("display") !== "grid") {
+//       clearElement.addClass("is-active");
+//     }
+//   } else {
+//     clearElement.removeClass("is-active");
+//   }
+// }
 
-  checkboxes.change(function () {
-    if (checkboxes.is(":checked")) {
-      $('[data-item="list-all"]').css("display", "grid");
-      $('[data-item="category"]').css("display", "none");
-      clearElement.removeClass("is-active");
-    } else {
-      $('[data-item="list-all"]').css("display", "none");
-      $('[data-item="category"]').css("display", "grid");
-      if ($('[data-item="list-all"]').css("display") !== "grid") {
-        clearElement.addClass("is-active");
-      }
-    }
-    adjustClearElementClass();
-    updateCountAndQuery();
-  });
+// checkboxes.change(function () {
+//   if (checkboxes.is(":checked")) {
+//     $('[data-item="list-all"]').css("display", "grid");
+//     $('[data-item="category"]').css("display", "none");
+//     clearElement.removeClass("is-active");
+//   } else {
+//     $('[data-item="list-all"]').css("display", "none");
+//     $('[data-item="category"]').css("display", "grid");
+//     if ($('[data-item="list-all"]').css("display") !== "grid") {
+//       clearElement.addClass("is-active");
+//     }
+//   }
+//   adjustClearElementClass();
+//   updateCountAndQuery();
+// });
 
-  searchBar.on("input", updateCountAndQuery);
+// searchBar.on("input", updateCountAndQuery);
 
-  clearElement.click(function () {
-    observer.disconnect();
-    $('[data-item="list-all"]').css("display", "none");
-    $('[data-item="category"]').css("display", "grid");
-    if ($('[data-item="list-all"]').css("display") !== "grid") {
-      $(this).addClass("is-active");
-    }
-    updateCountAndQuery();
-    adjustClearElementClass();
-    observer.observe(targetNode, config);
-  });
+// clearElement.click(function () {
+//   observer.disconnect();
+//   $('[data-item="list-all"]').css("display", "none");
+//   $('[data-item="category"]').css("display", "grid");
+//   if ($('[data-item="list-all"]').css("display") !== "grid") {
+//     $(this).addClass("is-active");
+//   }
+//   updateCountAndQuery();
+//   adjustClearElementClass();
+//   observer.observe(targetNode, config);
+// });
 
-  if (!checkboxes.is(":checked") && $('[data-item="list-all"]').css("display") !== "grid") {
-    clearElement.addClass("is-active");
-    $('[data-item="list-all"]').css("display", "none");
-    $('[data-item="category"]').css("display", "grid");
-    updateCountAndQuery();
-  }
-});
+// if (!checkboxes.is(":checked") && $('[data-item="list-all"]').css("display") !== "grid") {
+//   clearElement.addClass("is-active");
+//   $('[data-item="list-all"]').css("display", "none");
+//   $('[data-item="category"]').css("display", "grid");
+//   updateCountAndQuery();
+// }
+// });
